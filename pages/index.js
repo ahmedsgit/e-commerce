93cb99f121d40e00e11f0260d1c79d8e779765a9
ahmedsgit <acc.ahmedsazzad@gmail.com>
2021-11-02 11:41:29 +0600

    list of product

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@material-ui/core';
 import Layout from '../components/Layout';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Product</h1>
-      <ul>
-        <li>product 1</li>
-        <li>product 1</li>
-        <li>product 1</li>
-        <li>product 1</li>
-      </ul>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product)=>(
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia component="img" image={product.image} title ={product.name}/>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button>
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </Layout>
   );
 }
---
+++ utils/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            name:'Free Shirt',
+            category:'Shirts',
+            image:'images/shirts1.jpg',
+            price: 70,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            inStock:20,
+            desc: 'A popular Shirt'
+        },
+        {
+            name:'Fit Shirt',
+            category:'Shirts',
+            image:'images/shirts2.jpg',
+            price: 20,
+            brand:'Adidas',
+            rating: 5,
+            numReviews: 13,
+            inStock:20,
+            desc: 'A Fit Shirt'
+        },
+        {
+            name:'Slim Shirt',
+            category:'Shirts',
+            image:'images/shirts3.jpg',
+            price: 90,
+            brand:'Raymond',
+            rating: 4,
+            numReviews: 9,
+            inStock:20,
+            desc: 'A popular Shirt'
+        },
+        {
+            name:'Golf Pant',
+            category:'pants',
+            image:'images/pants1.jpg',
+            price: 70,
+            brand:'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            inStock:10,
+            desc: 'A popular Pant'
+        },
+        {
+            name:'Free Pant',
+            category:'pants',
+            image:'images/pants2.jpg',
+            price: 90,
+            brand:'Addidas',
+            rating: 5,
+            numReviews: 10,
+            inStock:20,
+            desc: 'A good looking pant'
+        },
+        {
+            name:'Classic pants',
+            category:'pants',
+            image:'images/pants3.jpg',
+            price: 70,
+            brand:'zara',
+            rating: 2.5,
+            numReviews: 10,
+            inStock:10,
+            desc: 'A popular pant'
+        },
+    ]
+}
+
+export default data;
